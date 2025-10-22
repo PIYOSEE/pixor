64f8b367cf732bea5524bfb584fce3e03549f318
PIYOSEE <piyosee7@gmail.com>
2025-10-22 22:36:30 +0530

    Fixed className error and updated gradient background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+
+
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import Result from './pages/Result'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gray-300'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/buycredit' element={<BuyCredit/>}/>
+        <Route path='/result' element={<Result/>}/>
       
+      </Routes>
     </div>
   )
 }
