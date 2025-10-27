a64471ad383c59fd7443b745dbf7fb4fc70bba72
PIYOSEE <piyosee7@gmail.com>
2025-10-27 14:15:56 +0530

    Update logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import { Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gray-300'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/buycredit' element={<BuyCredit/>}/>
