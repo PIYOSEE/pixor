06c3a96c7730cebe22e74880d8abba57b7f0ce36
PIYOSEE <piyosee7@gmail.com>
2025-10-28 13:52:40 +0530

    navbar update

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,40 @@
 import React, { useState } from 'react'
 import {assets} from '../assets/assets'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
-  const [ user , setUser ] = useState(null)
+  const [ user , setUser ] = useState(true)
+  const navigate = useNavigate()
 
   return (
     <div className='flex items-center justify-between py-4'>
-      <Link>
+      <Link to ='/'>
       <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'></img>
       </Link>
 
       <div>
         { user ?
-         <div></div> 
+         <div className='flex items-center gap-2 sm:gap-3'>
+          <button 
+            className='flex items-center gap-2 bg-blue-300 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-3'> 
+            <img className='w-5' src={assets.credit_star} alt=""/>
+            <p>Credits left : 50</p>
+          </button>
+          <p>Hi</p>
+          <div className='relative group'>
+            <img className='w-10 drop-shadow' src={assets.profile_icon} alt=""/>
+            <div className='absoulte hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+              <ul>
+                <li>Logout</li>
+              </ul>
+            </div>
+          </div>
+         </div> 
          : 
          <div className='flex items-center gap-2 sm:gap-5'>
-          <p className='cursor-pointer'>Pricing</p>
+          <p onClick={()=> navigate('/buycredit')} 
+            className='cursor-pointer'>Pricing
+          </p>
           <button className='text-white bg-zinc-900 px-6 py-2 sm:px-10 text-sm rounded-full'>Login</button>
          </div>
         }
