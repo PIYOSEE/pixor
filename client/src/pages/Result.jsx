7f8976789644884cd485d10ef6c285ce69eef702
PIYOSEE <piyosee7@gmail.com>
2025-11-08 11:20:53 +0530

    result section updated

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Result = () => {
   return (
-    <div>
-      Result Page
-    </div>
+
+    <form>
+      <div>
+        <div className='relative'>
+          <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
+          <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]'/>
+        </div>
+
+        <p>Loading.....</p>
+      </div>
+      <div>
+
+      </div>
+    </form>
   )
 }
 
