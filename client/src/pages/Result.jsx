f9ebc1d5e2f520c847064414f77fee1b3ace0bb4
PIYOSEE <piyosee7@gmail.com>
2025-11-08 12:17:00 +0530

    updated result section

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Result = () => {
   return (
 
-    <form>
+    <form className='flex flex-col min-h-[90vh] justify-center items-center '>
       <div>
         <div className='relative'>
           <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
@@ -13,8 +13,21 @@ const Result = () => {
 
         <p>Loading.....</p>
       </div>
-      <div>
+      <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-.02 mt-10 rounded-full'>
 
+        <input type="text" placeholder='Describe what you want to generate' 
+        className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'/>
+
+        <button type='submit'
+          class="bg-black text-white shadow-md hover:bg-gray-900 transition-transform hover:scale-105 px-10 sm:px-16 py-3 rounded-full">
+           Generate
+        </button>
+      </div>
+
+      <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
+        <p className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'>Generate Another</p>
+        <a href="#" download
+          className=' bg-zinc-900 px-10 py-3 rounded-full cursor-pointer'>Download</a>
       </div>
     </form>
   )
