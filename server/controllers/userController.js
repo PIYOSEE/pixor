2caa10bf450512f300f8e20233a4777a80c9af87
PIYOSEE <piyosee7@gmail.com>
2025-11-29 11:45:37 +0530

    complete userController file

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import userModel from '../models/userModel.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { cache } from 'react'
 
 const registerUser = async ( req , res )=> {
     try{
@@ -26,6 +27,33 @@ const registerUser = async ( req , res )=> {
 
         res.json({sucess:true, token , user: {name:user.name}})
 
+    }
+    catch (error){
+        console.log(error)
+        res.json({sucess:false, message: error.message})
+    }
+}
+
+const loginUser = async(req , res)=>{
+    try{
+        const {email ,password} = req.body;
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            return res({sucess:false , message:'User does not exist'})
+        }
+
+        const isMatch = await bcrypt.compare(password , user.password)
+
+
+        if(isMatch){
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+
+            res.json({sucess:true, token , user: {name:user.name}})
+        }
+        else{
+            return res({sucess:false , message:'Invalid credentials'})
+        }
     }
     catch (error){
         console.log(error)
