d8a610e1c53c5e958f016b6d0d88cf54c350f2cb
PIYOSEE <piyosee7@gmail.com>
2025-12-02 12:32:16 +0530

    added routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
 
+app.use('/api/user', userRouter)
 app.use(express.json())
 app.use(cors())
 
