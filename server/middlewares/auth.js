086c3d890e4fe023ae14df663f59ce8a72c2ee2e
PIYOSEE <piyosee7@gmail.com>
2025-12-07 11:49:16 +0530

    changes

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,8 +8,18 @@ const userAuth = async (req , res , next) =>{
         return res.json({ success: false , message : 'Not Authorized , Login Again!'});
     }
     try {
-        const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET)
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (tokenDecode.id){
+            req.body.userID = tokenDecode.id;
+        }else{
+            return res.json({ success: false , message : 'Not Authorized , Login Again!'});
+        }
+
+        next();
     } catch (error) {
-        
+        res.json({success:false , message: error.message})
     }
 }
+
+export default userAuth;
