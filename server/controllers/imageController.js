56c7d95b62ff47dc4121e3c7e817bd496df2b8dd
PIYOSEE <piyosee7@gmail.com>
2025-12-08 21:14:55 +0530

    updated

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,5 +1,6 @@
+import axios from "axios"
 import userModel from "../models/userModel"
-
+import formData from 'form-data'
 
 
 export const generateImage = async (req , res)=> {
@@ -14,6 +15,21 @@ export const generateImage = async (req , res)=> {
         if(user.creditBalance === 0 || userModel.creditBalance < 0 ){
             return res.json({success:false , message:'No Credit Balance' ,creditBalance:user.creditBalance })
         } 
+
+        const formData = new formData()
+        formData.append('prompt' , prompt)
+
+        const {data} = await axios.post(
+            'https://clipdrop-api.co/text-to-image/v1' ,
+             formData ,
+             {
+                headers: {'x-api-key': process.env.CLIPDROP_API,},
+                responseType : 'arraybuffer'
+             }
+            )
+
+        
+
     } 
     catch (error) {
         console.log(error.message)
